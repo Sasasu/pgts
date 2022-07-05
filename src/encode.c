a93f86bafac64a93f9a45b35b5dd6a2f8af9b065
Sasasu <i@sasa.su>
2022-07-05 16:40:39 +0800

    wip

--- src/encode.c
+++ src/encode.c
@@ -1,31 +1,8 @@
-#include <stdint.h>
-typedef double float64_t;
+#include "encode.h"
 
 #include <math.h>
-#include <stdlib.h>
 #include <string.h>
 #include <zstd.h> // for ZSTD support
-
-int _u8_encode(
-    uint64_t *input, size_t input_sz,		  //
-    unsigned char **output, size_t *output_sz,	  //
-    void *(*realloc_func)(void *, size_t, size_t) //
-);
-int _u8_decode(
-    unsigned char *input, size_t input_sz,	  //
-    uint64_t **output, size_t *output_sz,	  //
-    void *(*realloc_func)(void *, size_t, size_t) //
-);
-int _f8_encode(
-    float64_t *input, size_t input_sz,	       //
-    unsigned char **output, size_t *output_sz, //
-    void *(*alloc_func)(size_t, size_t)	       //
-);
-int _f8_decode(
-    float64_t *input, size_t input_sz,	       //
-    unsigned char **output, size_t *output_sz, //
-    void *(*alloc_func)(size_t, size_t)	       //
-);
 
 #define TE_VER_MASK 0b11000000 /* 4 binary version */
 #define TE__SZ_MASK 0b00110000 /* 4 type of size */
--- src/pgts.c
+++ src/pgts.c
@@ -1,23 +1,61 @@
 #include "c.h"
 #include "postgres.h"
 
+#include "catalog/pg_type_d.h"
 #include "datatype/timestamp.h" // for timestamp type
 #include "fmgr.h"		// for PG_FUNCTION_*
+#include "utils/array.h"
+
+#define ARRNELEMS(x) ArrayGetNItems(ARR_NDIM(x), ARR_DIMS(x))
+#define ARRPTR(x) ((uint64_t *)ARR_DATA_PTR(x))
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(i8_encode);
-Datum i8_encode(PG_FUNCTION_ARGS)
+#include "encode.h"
+
+static void *_realloc(void *p, size_t o, size_t n) { return repalloc(p, n); }
+
+PG_FUNCTION_INFO_V1(u8_encode);
+Datum u8_encode(PG_FUNCTION_ARGS)
 {
-	//
-	PG_RETURN_BYTEA_P(0);
+	ArrayType *in = PG_GETARG_ARRAYTYPE_P(0);
+	int32_t inn = ARRNELEMS(in);
+
+	uint8_t *out = NULL;
+	size_t outn = 0;
+
+	_u8_encode(ARRPTR(in), inn, &out, &outn, _realloc);
+
+	bytea *ret = palloc(VARHDRSZ + outn);
+	SET_VARSIZE(ret, VARHDRSZ + outn);
+	memcpy(VARDATA(ret), VARDATA_ANY(out), outn);
+
+	PG_RETURN_BYTEA_P(ret);
 }
 
-PG_FUNCTION_INFO_V1(i8_decode);
-Datum i8_decode(PG_FUNCTION_ARGS)
+PG_FUNCTION_INFO_V1(u8_decode);
+Datum u8_decode(PG_FUNCTION_ARGS)
 {
-	//
-	PG_RETURN_DATUM(0);
+	bytea *in = PG_GETARG_BYTEA_P(0);
+	int32_t inn = VARSIZE_ANY_EXHDR(in);
+
+	uint64_t *out = NULL;
+	size_t outn = 0;
+
+	_u8_decode((uint8_t *)VARDATA_ANY(in), inn, &out, &outn, _realloc);
+
+	int32_t nbytes = ARR_OVERHEAD_NONULLS(1) + sizeof(uint64_t) * outn;
+	ArrayType *ret = (ArrayType *) palloc0(nbytes);
+	SET_VARSIZE(ret, nbytes);
+	ARR_NDIM(ret) = 1;
+	ret->dataoffset = 0;
+	ARR_ELEMTYPE(ret) = INT8OID;
+	ARR_DIMS(ret)[0] = outn;
+	ARR_LBOUND(ret)[0] = 1;
+
+	memcpy(ARRPTR(ret), out, outn * sizeof(uint64_t));
+
+	PG_RETURN_ARRAYTYPE_P(ret);
 }
 
 PG_FUNCTION_INFO_V1(timestamp_encode);
@@ -29,20 +67,6 @@ Datum timestamp_encode(PG_FUNCTION_ARGS)
 
 PG_FUNCTION_INFO_V1(timestamp_decode);
 Datum timestamp_decode(PG_FUNCTION_ARGS)
-{
-	//
-	PG_RETURN_DATUM(0);
-}
-
-PG_FUNCTION_INFO_V1(f4_encode);
-Datum f4_encode(PG_FUNCTION_ARGS)
-{
-	//
-	PG_RETURN_BYTEA_P(0);
-}
-
-PG_FUNCTION_INFO_V1(f4_decode);
-Datum f4_decode(PG_FUNCTION_ARGS)
 {
 	//
 	PG_RETURN_DATUM(0);
